783ba3449558d735a69f272f8cca4be6b3862a95
Eliran Hadad <eliranhadad@gmail.com>
2021-07-22 18:28:14 +0300

    consumer - create a sql script for inserting data to db

---
+++ Consumer/develop/create_data.sql
@@ -0,0 +1,20 @@
+insert into hospital.medication_period_events
+(patient_id,medication_name,action_type, action_activation_date) 
+values
+	("Haim","Raphapen","START", "2021-6-29"),
+    ("Haim","Raphapen","START", "2021-11-8"),
+    ("Haim","Raphapen","START", "2021-9-25"),
+    ("Haim","Raphapen","CANCEL_START", "2021-12-23"),
+    ("Haim","Raphapen","CANCEL_START", "2021-1-30"),
+    ("Haim","Raphapen","STOP", "2021-4-27"),
+    ("Haim","Raphapen","STOP", "2021-8-7"),
+    ("Haim","Raphapen","CANCEL_STOP", "2021-5-19"),
+    ("Haim","Raphapen","START", "2021-7-15"),
+    ("Haim","Raphapen","STOP", "2021-5-29"),
+    ("Haim","Raphapen","START", "2021-3-8"),
+    ("Moshe","Raphapen","STOP", "2021-6-22"),
+    ("Haim","Acamol","START", "2021-6-22"),
+    ("Haim","Acamol","CANCEL_START", "2021-6-22");
+    
+    
+    
